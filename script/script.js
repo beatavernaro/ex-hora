7f7a2d793faeabb079e1c9fcd9aa158dc743f36a
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2022-01-26 11:22:04 -0300

    chore: usando `camelCase` somente por convenção do javascript https://eslint/docs/rules/camelcase

--- script/script.js
+++ script/script.js
@@ -4,7 +4,7 @@ function carregar() {
   const data = new Date();
   const hora = data.getHours();
   const fundo = document.querySelector("body#fundo");
-  const fundosection = document.querySelector("section#fundo-section");
+  const fundoSection = document.querySelector("section#fundo-section");
   const imagemManha = "style/manha.jpg";
   const imagemTarde = "style/tarde.jpg";
   const imagemNoite = "style/noite.jpg";
@@ -13,14 +13,14 @@ function carregar() {
   if (hora >= 5 && hora < 12) {
     imagem.style.backgroundImage = `url(${imagemManha})`;
     fundo.style.backgroundColor = "#FCD580";
-    fundosection.style.backgroundColor = "#BC8B12";
+    fundoSection.style.backgroundColor = "#BC8B12";
   } else if (hora >= 12 && hora < 18) {
     imagem.style.backgroundImage = `url(${imagemTarde})`;
     fundo.style.backgroundColor = "#FC8352";
-    fundosection.style.backgroundColor = "#FBB869";
+    fundoSection.style.backgroundColor = "#FBB869";
   } else {
     imagem.style.backgroundImage = `url(${imagemNoite})`;
     fundo.style.backgroundColor = "#042653";
-    fundosection.style.backgroundColor = "#3483DF";
+    fundoSection.style.backgroundColor = "#3483DF";
   }
 }
