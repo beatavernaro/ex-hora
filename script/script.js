e8c063f916a4f84f94046d6de9b447aeecf711d6
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2022-01-26 11:18:47 -0300

    chore: separando as imagens para facilitar a troca de imagens no futuro

--- script/script.js
+++ script/script.js
@@ -5,18 +5,21 @@ function carregar() {
   const hora = data.getHours();
   const fundo = document.querySelector("body#fundo");
   const fundosection = document.querySelector("section#fundo-section");
+  const imagemManha = "style/manha.jpg";
+  const imagemTarde = "style/tarde.jpg";
+  const imagemNoite = "style/noite.jpg";
 
   msg.innerHTML = `<strong> Agora são ${hora} horas. </strong>`;
   if (hora >= 5 && hora < 12) {
-    imagem.style.backgroundImage = "url(style/manha.jpg)";
+    imagem.style.backgroundImage = `url(${imagemManha})`;
     fundo.style.backgroundColor = "#FCD580";
     fundosection.style.backgroundColor = "#BC8B12";
   } else if (hora >= 12 && hora < 18) {
-    imagem.style.backgroundImage = "url(style/tarde.jpg)";
+    imagem.style.backgroundImage = `url(${imagemTarde})`;
     fundo.style.backgroundColor = "#FC8352";
     fundosection.style.backgroundColor = "#FBB869";
   } else {
-    imagem.style.backgroundImage = "url(style/noite.jpg)";
+    imagem.style.backgroundImage = `url(${imagemNoite})`;
     fundo.style.backgroundColor = "#042653";
     fundosection.style.backgroundColor = "#3483DF";
   }
